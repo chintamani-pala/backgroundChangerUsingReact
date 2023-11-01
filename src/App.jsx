4bfd62a0b6db5f94225c43b7a6281627c15941aa
CHINTAMANI PALA <chintamanipala67@gmail.com>
2023-11-01 20:54:16 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
 import { useState } from 'react';
 
 function App() {
-  const [BgColor, setBgColor] = useState('bg-gray-800')
+  const [BgColor, setBgColor] = useState('bg-gray-800 duration-100')
 
   return (
     <>
-       <div className={`${BgColor} duration-100 w-screen h-screen  flex items-center justify-center flex-col`}>
+       <div className={`${BgColor} w-screen h-screen  flex items-center justify-center flex-col`}>
          <div className="flex">
            <button 
            className="bg-red-500 p-4 m-2 rounded"
-           onClick = {() => setBgColor('bg-red-500')}
+           onClick = {() => setBgColor('bg-red-500 duration-100')}
            >Red</button>
            <button 
            className="bg-blue-500 p-4 m-2 rounded"
-           onClick={() => setBgColor('bg-blue-500')}
+           onClick={() => setBgColor('bg-blue-500 duration-100')}
            >Blue</button>
            <button 
            className="bg-yellow-500 p-4 m-2 rounded"
-           onClick={() => setBgColor('bg-yellow-500')}
+           onClick={() => setBgColor('bg-yellow-500 duration-100')}
            >Yellow</button>
            <button 
            className="bg-green-500 p-4 m-2 rounded"
-           onClick={() => setBgColor('bg-green-500')}
+           onClick={() => setBgColor('bg-green-500 duration-100')}
            >Green</button>
          </div>
          <div className="text-center text-white mt-4">Created by Chintamani Pala</div>

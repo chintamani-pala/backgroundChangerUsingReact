0366db706aacfbd3d523ed59b2e7ea32f53a0026
chintamani pala <chintamanipala67@gmail.com>
2023-11-01 19:53:28 +0530

    Add all files

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+function App() {
+  const [BgColor, setBgColor] = useState('bg-gray-800')
+
+  return (
+    <>
+       <div className={`${BgColor} w-screen h-screen  flex items-center justify-center flex-col`}>
+         <div className="flex">
+           <button 
+           className="bg-red-500 p-4 m-2 rounded"
+           onClick = {() => setBgColor('bg-red-500')}
+           >Red</button>
+           <button 
+           className="bg-blue-500 p-4 m-2 rounded"
+           onClick={() => setBgColor('bg-blue-500')}
+           >Blue</button>
+           <button 
+           className="bg-yellow-500 p-4 m-2 rounded"
+           onClick={() => setBgColor('bg-yellow-500')}
+           >Yellow</button>
+           <button 
+           className="bg-green-500 p-4 m-2 rounded"
+           onClick={() => setBgColor('bg-green-500')}
+           >Green</button>
+         </div>
+         <div className="text-center text-white mt-4">Created by Chintamani Pala</div>
+       </div>
+    </>
+  )
+}
+
+export default App
